59756649cc757502ddf9369c3370c5607e3f66cc
Daniel Tevev <79971422+tevevdani@users.noreply.github.com>
2022-03-15 18:55:37 +0100

    Add files via upload

---
+++ Service/DTO/ClubDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Data;
+
+namespace Service.DTO
+{
+    public class ClubDTO
+    {
+        public ClubDTO(Club c)
+        {
+            Id = c.id;
+            name = c.name;
+            owner = c.owner;
+            city = c.city;
+            country = c.country;
+        }
+
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string owner { get; set; }
+        public string city { get; set; }
+        public string country { get; set; }
+    }
+}
---
+++ Service/DTO/PlayerDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Data;
+
+namespace Service.DTO
+{
+    public class PlayerDTO
+    {
+        public PlayerDTO(Player p)
+        {
+            Id = p.id;
+            firstName = p.firstName;
+            lastName = p.lastName;
+            signingDate = p.signingDate;
+            rank = p.rank;
+            totalGoals = p.totalGoals;
+            clubId = p.clubId;
+        }
+
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string signingDate { get; set; }
+        public string rank { get; set; }
+        public string totalGoals { get; set; }
+        public int clubId { get; set; }
+
+        public virtual ClubDTO club { get; set; }
+
+    }
+}
